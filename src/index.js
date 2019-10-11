b225335bbfc8fe75929e0afe64433a74a4ce1bfe
danale <danale@danales-MacBook-Pro.local>
2019-10-11 15:37:59 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = 'mongodb+srv://admin:iDA@3eJ%233@cluster0-nnrku.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Mongo Instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to Mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
