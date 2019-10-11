cef34efd3f1d15835df509a12932e10fea47bf36
danale <danale@danales-MacBook-Pro.local>
2019-10-11 15:46:06 -0500

    Modified mongo instance

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('./models/User');
-require('./models/Track');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const trackRoutes = require('./routes/trackRoutes');
-const requireAuth = require('./middlewares/requireAuth');
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
@@ -13,22 +13,23 @@ app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(trackRoutes);
 
-const mongoUri = 'mongodb+srv://admin:iDA@3eJ%233@cluster0-nnrku.mongodb.net/test?retryWrites=true&w=majority';
+const mongoUri =
+  "mongodb+srv://<username>:<password>@cluster0-nnrku.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-mongoose.connection.on('connected', () => {
-  console.log('Connected to Mongo Instance');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongo Instance");
 });
-mongoose.connection.on('error', (err) => {
-  console.error('Error connecting to Mongo', err);
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to Mongo", err);
 });
 
-app.get('/', requireAuth, (req, res) => {
+app.get("/", requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
